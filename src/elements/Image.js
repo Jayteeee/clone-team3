053a1bf040366957f92950a2b940b45effa67205
Jayteeee <wjdxo9928@gmail.com>
2022-04-15 23:58:22 +0900

    start!!!

---
+++ src/elements/Image.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+import React from "react";
+
+const Image = (props) => {
+  const { shape, src, size, half, center } = props;
+
+  const styles = {
+    src: src,
+    size: size,
+    half,
+    center,
+  };
+
+  if (shape === "circle") {
+    return <ImageCircle {...styles}></ImageCircle>;
+  }
+
+  if (shape === "rectangle") {
+    return (
+      <AspectOutter>
+        <AspectInner {...styles}></AspectInner>
+      </AspectOutter>
+    );
+  }
+
+  if (shape === "big_squre") {
+    return <BigSquareImage {...styles}></BigSquareImage>;
+  }
+
+  return (
+    <React.Fragment>
+      <ImageDefault {...styles}></ImageDefault>
+    </React.Fragment>
+  );
+};
+
+Image.defaultProps = {
+  shape: "circle",
+  src: "https://previews.123rf.com/images/aquir/aquir1311/aquir131100316/23569861-sample-grunge-red-round-stamp.jpg",
+  size: 36,
+};
+
+const ImageDefault = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+const AspectOutter = styled.div`
+  margin: auto;
+  width: 100%;
+  min-width: 250px;
+`;
+
+const AspectInner = styled.div`
+  position: relative;
+  padding-top: 75%;
+  overflow: hidden;
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+`;
+
+const ImageCircle = styled.div`
+  --size: ${(props) => props.size}px;
+  width: var(--size);
+  height: var(--size);
+  border-radius: var(--size);
+
+  background-image: url("${(props) => props.src}");
+  background-size: cover;
+  margin: 4px;
+`;
+
+const BigSquareImage = styled.img`
+  width: 100%;
+  ${(props) => (props.half ? `flex-basis: 50%;` : "")}
+  ${(props) => (props.center ? `flex-basis: 50%;` : "")}
+  min-width: 250px;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  margin: 10px 0;
+  @media (max-width: 280px) {
+    min-width: 150px;
+  }
+  @media (max-width: 360px) {
+    min-width: 180px;
+  }
+`;
+
+export default Image;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Router } from "react-router-dom";
+import Main from "Main";
+import articleDetail from "articleDetail";
+import articleList from "articleList";
+import articleWrite from "articleWrite";
+import Chat from "Chat";
+import Login from "Login";
+import Signup from "Signup";
+import myPage from "myPage";
+import Header from "Header";
+import Footer from "Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Router path="/" exact component={Main} />
+      <Router path="/signup" exact component={Signup} />
+      <Router path="/login" exact component={Login} />
+      <Router path="/" exact component={articleDetail} />
+      <Router path="/" exact component={articleList} />
+      <Router path="/" exact component={articleWrite} />
+      <Router path="/" exact component={Chat} />
+      <Router path="/" exact component={myPage} />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
