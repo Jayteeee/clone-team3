87047eb038ae8463635056a96699eab063c27ef9
kimyeongkyung <klk17625@naver.com>
2022-04-16 16:33:47 +0900

    package.json과 app.js 통일

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,34 +1,31 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-// import Main from "../pages/Main";
-// import articleDetail from "../pages/articleDetail";
-// import articleList from "../pages/articleList";
-// import articleWrite from "../pages/articleWrite";
+import Main from "../pages/Main";
+import articleDetail from "../pages/articleDetail";
+import articleList from "../pages/articleList";
+import articleWrite from "../pages/articleWrite";
 import Chat from "../pages/Chat";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import myPage from "../pages/myPage";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header />
-      <Router path="/" exact component={Main} />*/}
-
+      <Header />
+      <Route path="/main" exact component={Main} />
       <Route path="/signup" exact component={Signup} />
       <Route path="/login" exact component={Login} />
-      {/*
-      <Router path="/" exact component={articleDetail} />
-      <Router path="/" exact component={articleList} />
-      <Router path="/" exact component={articleWrite} />*/}
+      <Route path="/detail/:articleNumber" exact component={articleDetail} />
+      <Route path="/list" exact component={articleList} />
+      <Route path="/add" exact component={articleWrite} />
+      <Route path="/edit/:articleNumber" exact component={articleWrite} />
       <Route path="/chat" exact component={Chat} />
       <Route path="/mypage" exact component={myPage} />
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 }
-
 export default App;
