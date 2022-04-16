e587ccef8f9b87b3f35308272b6a696623195a29
Jayteeee <wjdxo9928@gmail.com>
2022-04-16 16:33:10 +0900

    패키지, app.js 통일

--- src/shared/App.js
+++ src/shared/App.js
@@ -2,31 +2,30 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Main from "../pages/Main";
-// import articleDetail from "../pages/articleDetail";
+import articleDetail from "../pages/articleDetail";
 import articleList from "../pages/articleList";
-// import articleWrite from "../pages/articleWrite";
-// import Chat from "../pages/Chat";
-// import Login from "../pages/Login";
-// import Signup from "../pages/Signup";
-// import myPage from "../pages/myPage";
+import articleWrite from "../pages/articleWrite";
+import Chat from "../pages/Chat";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import myPage from "../pages/myPage";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route path="/" exact component={Main} />
-      {/* <Route path="/signup" exact component={Signup} />
+      <Route path="/main" exact component={Main} />
+      <Route path="/signup" exact component={Signup} />
       <Route path="/login" exact component={Login} />
-      <Route path="/" exact component={articleDetail} /> */}
-      <Route path="/article" exact component={articleList} />
-      {/* <Route path="/" exact component={articleWrite} />
-      <Route path="/" exact component={Chat} />
-      <Route path="/" exact component={myPage} /> */}
+      <Route path="/detail/:articleNumber" exact component={articleDetail} />
+      <Route path="/list" exact component={articleList} />
+      <Route path="/add" exact component={articleWrite} />
+      <Route path="/edit/:articleNumber" exact component={articleWrite} />
+      <Route path="/chat" exact component={Chat} />
+      <Route path="/mypage" exact component={myPage} />
       <Footer />
     </BrowserRouter>
   );
 }
-
 export default App;
